8af029f682689d4e59889144cb5d651c57b122c9
Marco Grier <mlgrier@gmail.com>
2017-07-19 20:32:02 -0500

    Update

--- PhotoLax/ViewController.swift
+++ PhotoLax/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
         return cell
     }
 
